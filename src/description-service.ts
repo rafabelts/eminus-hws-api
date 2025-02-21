9c2eaff89586390f2a9882a61a0481d55f58f65d
Rafael Beltran <rafabeltrans17@gmail.com>
2025-02-21 12:42:50 -0600

    removed dotenv import

--- src/description-service.ts
+++ src/description-service.ts
@@ -1,21 +1,18 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import * as dotenv from "dotenv";
 
 export const descriptionService = async (description: string, env: any) => {
-    dotenv.config();
+  const apiKey = env.GEMINI_KEY;
+  console.log(apiKey);
 
-    const apiKey = env.GEMINI_KEY;
-    console.log(apiKey);
+  if (!apiKey) {
+    throw new Error("No API Key found");
+  }
 
-    if (!apiKey) {
-        throw new Error("No API Key found");
-    }
+  const genAi = new GoogleGenerativeAI(apiKey);
 
-    const genAi = new GoogleGenerativeAI(apiKey);
+  const model = genAi.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-    const model = genAi.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-    const prompt = `Por favor, describe con bastante detalle la descripción de mi tarea. 
+  const prompt = `Por favor, describe con bastante detalle la descripción de mi tarea. 
 No olvides agregar si hay alguna instrucción importante del profesor. 
 Dame la respuesta usando las etiquetas <p> para los párrafos, <strong> para resaltar información importante y <em> para cursivas. 
 Ejemplo de salida esperada:
@@ -27,12 +24,12 @@ Solo devuelve el contenido en formato HTML sin ningún otro texto adicional.
 
 Descripción: ${description}`;
 
-    try {
-        const result = await model.generateContent(prompt);
-        const response = await result.response.text();
+  try {
+    const result = await model.generateContent(prompt);
+    const response = await result.response.text();
 
-        return response;
-    } catch (error) {
-        return "Error. No se encontro la key de Gemini";
-    }
+    return response;
+  } catch (error) {
+    return "Error. No se encontro la key de Gemini";
+  }
 };
